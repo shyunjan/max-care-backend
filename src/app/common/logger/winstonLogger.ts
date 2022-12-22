a364ee6de81e77eef36795ddca61f66d563cfbb8
Shin Hyunjae <snhj3827@itechcompany.kr>
2022-12-22 17:49:57 +0900

    fix winton logger

--- src/app/common/logger/winstonLogger.ts
+++ src/app/common/logger/winstonLogger.ts
@@ -9,7 +9,7 @@ const {
   printf,
   // prettyPrint,
   colorize,
-  uncolorize,
+  // uncolorize,
   // simple,
 } = format;
 const colorizer = colorize();
@@ -23,11 +23,11 @@ const createLogger = () => {
   }
 
   logger = winston.createLogger({
-    level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
+    level: process.env.NODE_ENV === 'production' ? 'verbose' : 'debug',
     format: combine(
       errors({ stack: true }),
-      // timestamp({ format: 'YYYY-MM-dd HH:mm:ss:SSS' })
-      timestamp({ format: 'HH:mm:ss:SSS' })
+      timestamp({ format: 'YYYY-MM-dd HH:mm:ss:SSS' })
+      // timestamp({ format: 'HH:mm:ss:SSS' })
       // json()
       // ...(process.env.NODE_ENV !== 'production' ? [prettyPrint()] : [])
     ),
@@ -66,31 +66,43 @@ const createLogger = () => {
 
     /* Production phase일 경우는 파일로 로그를 저장. AWS일 경우는 파일이나 컬러가 필요없으므로 수정 요망. */
   } else {
-    const fileLogFormat = combine(
-      uncolorize(),
-      printf(({ level, message, timestamp, from }) => `${level} ${timestamp} [${from}] ${message}`)
-    );
     logger.add(
-      new winston.transports.DailyRotateFile({
-        level: 'error',
-        filename: 'log/error-%DATE%.log',
-        datePattern: 'YYYY-MM-DD',
-        zippedArchive: true,
-        maxSize: '1k',
-        maxFiles: '14d',
-        format: fileLogFormat,
+      new winston.transports.Console({
+        format: combine(
+          label({ label: 'Max-Care' }),
+          printf(
+            (info: { level: string; message: any; [key: string]: string }) =>
+              `[${info.label}] ${info.level} ${info.timestamp} [${info.from}] ${info.message}`
+          )
+        ),
       })
     );
-    logger.add(
-      new winston.transports.DailyRotateFile({
-        filename: 'log/application-%DATE%.log',
-        datePattern: 'YYYY-MM-DD',
-        zippedArchive: true,
-        maxSize: '1k',
-        maxFiles: '14d',
-        format: fileLogFormat,
-      })
-    );
+
+    // const fileLogFormat = combine(
+    //   uncolorize(),
+    //   printf(({ level, message, timestamp, from }) => `${level} ${timestamp} [${from}] ${message}`)
+    // );
+    // logger.add(
+    //   new winston.transports.DailyRotateFile({
+    //     level: 'error',
+    //     filename: 'log/error-%DATE%.log',
+    //     datePattern: 'YYYY-MM-DD',
+    //     zippedArchive: true,
+    //     maxSize: '1k',
+    //     maxFiles: '14d',
+    //     format: fileLogFormat,
+    //   })
+    // );
+    // logger.add(
+    //   new winston.transports.DailyRotateFile({
+    //     filename: 'log/application-%DATE%.log',
+    //     datePattern: 'YYYY-MM-DD',
+    //     zippedArchive: true,
+    //     maxSize: '1k',
+    //     maxFiles: '14d',
+    //     format: fileLogFormat,
+    //   })
+    // );
   }
 };
 
